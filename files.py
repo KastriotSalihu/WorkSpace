d4830d4a8265e0723e10a6853e21e14d9cc36af1
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2020-06-20 20:17:58 +0200

    Update files.py

--- files.py
+++ files.py
@@ -6,45 +6,38 @@ target_folder='source'
 file_name_to_save_to='cpp'
 file_extension='.cpp'
 module_for_file_making='files.py'
-
+file=f'{file_name_to_save_to}{file_extension}'
 
 def getOriginalFileNameAndPath(workingDirectory):
     #returns full path to file for workspace, based on the folder name
     file_name = os.path.basename(workingDirectory)
-    return workingDirectory+'\\'+ file_name + file_extension
+    return f'{workingDirectory}\\{file_name}{file_extension}'
 
-
-def changeToSourceFolder(workingDirectory, target_folder=target_folder):
+def changeToSourceFolder(target_folder=target_folder):
     #change directory to a target folder, such as 'source'
-    os.chdir(workingDirectory)
-    try:
+    if(os.path.isdir(f'./{target_folder}')):
         os.chdir(target_folder)
-        workingDirectory = f'{workingDirectory}\\{target_folder}'
-    except FileNotFoundError:
+    else:
         print(f"No '{target_folder}' dircetory.")
-    return workingDirectory
 
 def getNewFileName(newfile):
     #generates numbered name of the file to save to
-    for i in range(1,20):
-        try:
-            f = open(newfile,'r')
-            f.close()
-            if (i==20):
-                print("Limit reached!")
-            newfile = f'{file_name_to_save_to}{i}{file_extension}'
-        except FileNotFoundError:
+    i=1
+    while True:
+        if(os.path.isfile(f'./{newfile}')):
+           newfile=f'{file_name_to_save_to}{i}{file_extension}'
+           i=i+1
+        else:
             f = open(newfile,'w')
             f.close()
             break
     return newfile
 
-def getNewFilePath(workingDirectory):
+def getNewFilePath():
     #returns full path to file-to-save-to
-    workingDirectory = changeToSourceFolder(workingDirectory)
-    newfile = f'{file_name_to_save_to}{file_extension}'
-    newfile = getNewFileName(newfile)    
-    return str(workingDirectory) + '\\' + newfile
+    changeToSourceFolder()
+    newfile = getNewFileName(file)    
+    return f'{os.getcwd()}\\{newfile}'
 
 def compileCpp(basename):
     CMDcommand = f'g++ "{basename}" || cmd /k'
@@ -53,7 +46,7 @@ def compileCpp(basename):
 def saveContentToFile(base_file):
     save = input("Would you like to save? (y / n)")
     if (save == 'y'):
-        target_file=getNewFilePath(os.getcwd())
+        target_file=getNewFilePath()
         copyfile(base_file,target_file)
 
 
